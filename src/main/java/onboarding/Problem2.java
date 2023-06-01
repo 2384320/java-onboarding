4e4d18383efaebe1b86144195f41cfbb55842ef6
2384320 <chdms1549@gmail.com>
2023-06-01 23:14:15 +0900

    [Upload] Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char currentCharacter = cryptogram.charAt(i);
+            if (sb.length() == 0) {
+                sb.append(currentCharacter);
+                continue;
+            }
+
+            char previousCharacter = sb.charAt(sb.length() - 1);
+
+            if (previousCharacter == currentCharacter) sb.deleteCharAt(sb.length() - 1);
+            else sb.append(currentCharacter);
+        }
+        answer = sb.toString();
+
         return answer;
     }
 }
