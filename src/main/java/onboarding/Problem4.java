0e3db7a72a2014ceae92f326342baf5d92e6c31a
2384320 <chdms1549@gmail.com>
2023-06-01 23:34:35 +0900

    [Upload] Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (char alphabet : word.toCharArray()) {
+            if (alphabet == ' ') {
+                sb.append(' ');
+                continue;
+            }
+
+            if (Character.isLowerCase(alphabet)) alphabet = (char)('z' - alphabet + 'a');
+            else alphabet = (char)('Z' - alphabet + 'A');
+            sb.append(alphabet);
+        }
+
+        answer = sb.toString();
         return answer;
     }
 }
