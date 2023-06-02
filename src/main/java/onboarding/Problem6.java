b552bec67cfb3df1e6f1f1c8fe9982d87ce02802
2384320 <chdms1549@gmail.com>
2023-06-02 17:40:30 +0900

    [Fix Feature] main 코드 가독성을 위해 되도록 모든 기능을 함수로 만듦
    1. getDuplicateWord
    - 닉네임을 두 글자씩 나눠 부분 문자열 생성
    - 부분 문자열은 중복 숫자와 함께 duplicateWordMap에 저장
    
    2. isDuplicatePart
    - 부분 문자열이 중복되면 true, 그렇지 않으면 false를 반환
    
    3. isDuplicateNickname
    - 닉네임이 부분 문자열을 포함한다면 true, 그렇지 않으면 false를 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,67 +3,57 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
-    private static HashMap<String, Integer> duplicateWordMap;
-    private static HashMap<String, String> rightNicknameAndEmail;
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
-        duplicateWordMap = new HashMap<>();
-        rightNicknameAndEmail = new HashMap<>();
-
-        // 이메일과 닉네임을 검사
-        // 규격에 맞는 양식이면 rightNicknameAndEmail 해시맵에 저장
-        for (int i = 0; i < forms.size(); i++) {
-            String nickname = forms.get(i).get(1);
-            String email = forms.get(i).get(0);
-            if (!checkRightNickname(nickname)
-                    || !checkRightEmail(email)) continue;
-            rightNicknameAndEmail.put(nickname, email);
-        }
+        HashMap<String, Integer> duplicateWordMap = new HashMap<>();
 
         // 모든 닉네임에서의 부분 문자열을 구함.
         // 중복 부분 문자열이 나온다면 나온 수 만큼 카운팅
-        getDuplicateWord();
-
-        for (String nickname : rightNicknameAndEmail.keySet()) {
-            if (isDuplicateNickname(nickname)) answer.add(rightNicknameAndEmail.get(nickname));
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            getDuplicateWord(nickname, duplicateWordMap);
         }
 
+        // 부분 문자열을 기준으로 탐색 시작
+        for (String duplicatePart : duplicateWordMap.keySet()) {
+            // 부분 문자열이 중복되었는지를 판단하며, 중복되지 않았다면 패스
+            if (!isDuplicatePart(duplicateWordMap.get(duplicatePart))) continue;
+            //
+            for (List<String> form : forms) {
+                // 닉네임이 해당 문자열을 포함한다면, 이메일을 리스트에 저장
+                if (isDuplicateNickname(form.get(1), duplicatePart)) answer.add(form.get(0));
+            }
+        }
         Collections.sort(answer);
+
         return answer;
     }
 
-    public static boolean isDuplicateNickname(String nickname) {
-        for (String duplicateWord : duplicateWordMap.keySet()) {
-            if (duplicateWordMap.get(duplicateWord) > 1) {
-                if (nickname.contains(duplicateWord)) return true;
-            }
+    private static boolean isDuplicateNickname(String nickname, String duplicatePart) {
+        // 닉네임이 부분 문자열을 포함한다면 true
+        return nickname.contains(duplicatePart);
+    }
+
+    private static boolean isDuplicatePart(int duplicateCount) {
+        // 부분 문자열 중복 숫자가 1 초과라면 true 반환
+        return duplicateCount > 1;
+    }
+
+    private static void getDuplicateWord(
+            String nickname,
+            HashMap<String, Integer> duplicateWordMap
+    ) {
+        // 닉네임을 두 글자씩 나눠 부분 문자열 생성
+        // 부분 문자열은 중복 숫자와 함께 duplicateWordMap에 저장
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String duplicatePart = nickname.substring(i, i+2);
+            duplicateWordMap.put(
+                    duplicatePart,
+                    duplicateWordMap.getOrDefault(
+                            duplicatePart,
+                            0
+                    ) + 1
+            );
         }
-        return false;
-    }
-
-    public static void getDuplicateWord() {
-        for (String nickname : rightNicknameAndEmail.keySet()) {
-            for (int i = 0; i < nickname.length() - 1; i++) {
-                String duplicatePart = nickname.substring(i, i+2);
-                duplicateWordMap.put(duplicatePart, duplicateWordMap.getOrDefault(duplicatePart, 0) + 1);
-            }
-        }
-    }
-
-    public static boolean checkRightNickname(String nickname) {
-        int nicknameLength = nickname.length();
-        //
-        if (nicknameLength < 1 || nicknameLength >= 20) return false;
-        else return nickname.matches("^[ㄱ-ㅎ가-힣]*$");
-    }
-
-    public static boolean checkRightEmail(String email) {
-        int emailLength = email.length();
-        if (!email.contains("@") || emailLength < 11 || emailLength >= 20) return false;
-
-        String[] idAndDomain = email.split("@");
-        if (!idAndDomain[1].equals("email.com")) return false;
-
-        return true;
     }
 }
