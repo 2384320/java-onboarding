ac87d5deff3de23f67f83ca3ce5537de02f272bf
2384320 <chdms1549@gmail.com>
2023-06-02 19:54:02 +0900

    [Fix Feature] main 코드 가독성을 위해 되도록 모든 기능을 함수로 만듦
    1. getTimeLineVisitCount
    - 타임라인 방문자에게 알고리즘 점수 +1 할당
    
    2. getFriendRelation
    - friend2를 friend1의 친구 목록에 저장
    
    3. isUserFriend
    - friend가 user의 친구인지 확인 -> 친구가 맞으면 true
    
    4. getFriendRelationCount
    - 친구 목록에 있는 모든 친구에게 +10 할당
    
    5. friendAlgorithmScoreSelect
    - 알고리즘 점수가 높은 상위 5개 데이터만 반환
    만약 5개 미만이라면 있는 만큼만 반환함

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,73 +4,105 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = new ArrayList<>();
+        List<String> answer;
         HashMap<String, Integer> friendAlgorithmScore = new HashMap<>();
-        HashMap<String, Integer> duplicateFriendHashMap = new HashMap<>();
+        HashMap<String, List<String>> friendRelation = new HashMap<>();
+        PriorityQueue<String[]> friendAlgorithmScoreSortQueue = new PriorityQueue<>((o1, o2) -> {
+            if (Integer.parseInt(o1[1]) == Integer.parseInt(o2[1])) return o1[0].compareTo(o2[0]);
+            return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);
+        });
 
-        friendAlgorithmScore = getTimeLineVisitCount(friendAlgorithmScore, visitors);
-        List<String> userFriends = getUserFriend(user, friends);
-
-        for (String userFriend : userFriends) {
-            List<String> friendsList = getUserFriend(userFriend, friends);
-
-            for (String friendsFriend : friendsList) {
-                duplicateFriendHashMap.put(friendsFriend, duplicateFriendHashMap.getOrDefault(friendsFriend, 0) + 1);
-            }
+        // visitors를 통해 방문 점수 파악하기
+        for (String visitor : visitors) {
+            getTimeLineVisitCount(friendAlgorithmScore, visitor);
         }
 
-        friendAlgorithmScore = getTogetherFriendCount(friendAlgorithmScore, duplicateFriendHashMap);
+        // friends를 통해 친구 관계 파악하기
+        // 이 결과는 friendRelation에 저장됨.
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+            getFriendRelation(friend1, friend2, friendRelation);
+            getFriendRelation(friend2, friend1, friendRelation);
+        }
+
+        for (String friend : friendRelation.keySet()) {
+            // 해당 친구가 user와 친구라면 getFriendRelationCount()를 거치도록 함.
+            // getFriendRelationCount(): 그의 친구에게 + 10 점수를 부여함.
+            if (!isUserFriend(friendRelation, friend, user) || friend.equals(user)) continue;
+            getFriendRelationCount(friendAlgorithmScore, friendRelation.get(friend));
+        }
 
         for (String friend : friendAlgorithmScore.keySet()) {
-            if (friend.equals(user)) friendAlgorithmScore.remove(friend);
-            else if (userFriends.contains(friend)) friendAlgorithmScore.remove(friend);
+            // 해당 친구가 user 본인이거나, user와 친구라면 패스하도록 함.
+            if (isUserFriend(friendRelation, friend, user) || friend.equals(user)) continue;
+            // 위의 조건에 충족하지 않으면
+            // 정렬을 위해 friendAlgorithmScoreSortQueue에 추가
+            friendAlgorithmScoreSortQueue.offer(new String[] {friend, Integer.toString(friendAlgorithmScore.get(friend))});
         }
-        System.out.println(friendAlgorithmScore);
 
+        answer = friendAlgorithmScoreSelect(friendAlgorithmScoreSortQueue);
         return answer;
     }
 
-    public static HashMap<String, Integer> getTogetherFriendCount(
-            HashMap<String, Integer> friendAlgorithmScore,
-            HashMap<String, Integer> duplicateFriendHashMap
+    private static List<String> friendAlgorithmScoreSelect(
+            PriorityQueue<String[]> friendAlgorithmScoreSortQueue
     ) {
-
-        for (String duplicateFriend : duplicateFriendHashMap.keySet()) {
-            friendAlgorithmScore.put(
-                    duplicateFriend,
-                    friendAlgorithmScore.getOrDefault(
-                            duplicateFriend,
-                            duplicateFriendHashMap.get(duplicateFriend)
-                    ) * 10
-            );
+        // friendAlgorithmScoreSortQueue에서 정렬되었던 값 중
+        // 상위 5개만 selectResult에 저장되도록 함.
+        List<String> selectResult = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            // friendAlgorithmScoreSortQueue가 비었다면 break;
+            if (friendAlgorithmScoreSortQueue.isEmpty()) break;
+            selectResult.add(friendAlgorithmScoreSortQueue.poll()[0]);
         }
-
-        return friendAlgorithmScore;
+        return selectResult;
     }
 
-    public static List<String> getUserFriend(String user, List<List<String>> friends) {
-        List<String> userFriend = new ArrayList<>();
-        for (int i = 0; i < friends.size(); i++) {
-            if (friends.get(i).contains(user)) {
-                int friendIndex = friends.get(i).indexOf(user) == 0 ? 1 : 0;
-                userFriend.add(friends.get(i).get(friendIndex));
-            }
-        }
-        return userFriend;
-    }
-
-    public static HashMap<String, Integer> getTimeLineVisitCount(
+    private static void getFriendRelationCount(
             HashMap<String, Integer> friendAlgorithmScore,
-            List<String> visitors
+            List<String> friends
     ) {
-
-        for (String visitor : visitors) {
+        // friends(친구 목록)에 있는 모든 친구에게 + 10
+        for (String friend : friends) {
             friendAlgorithmScore.put(
-                    visitor,
-                    friendAlgorithmScore.getOrDefault(visitor, 0) + 1
+                    friend,
+                    friendAlgorithmScore.getOrDefault(friend, 0) + 10
             );
         }
+    }
 
-        return friendAlgorithmScore;
+    private static boolean isUserFriend(
+            HashMap<String, List<String>> friendRelation,
+            String friend,
+            String user
+    ) {
+        // friend가 user의 친구인지 확인 -> 친구가 맞으면 true
+        return friendRelation.get(user).contains(friend);
+    }
+
+    private static void getFriendRelation(
+            String friend1,
+            String friend2,
+            HashMap<String, List<String>> friendRelation
+    ) {
+        // friend2를 friend1의 친구 목록에 저장함.
+        List<String> friendList = new ArrayList<>();
+        // 친구목록이 존재하지 않았던 friend1이라면 friend1 항목 초기화
+        if (friendRelation.containsKey(friend1)) friendList = friendRelation.get(friend1);
+        // friend2 친구를 친구목록에 저장
+        friendList.add(friend2);
+        friendRelation.put(friend1, friendList);
+    }
+
+    private static void getTimeLineVisitCount(
+            HashMap<String, Integer> friendAlgorithmScore,
+            String visitor
+    ) {
+        // 타임라인 방문자에게 + 1
+        friendAlgorithmScore.put(
+                visitor,
+                friendAlgorithmScore.getOrDefault(visitor, 0) + 1
+        );
     }
 }
