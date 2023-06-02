674be1edc088ef1858932945c51593ba2a8a1efe
2384320 <chdms1549@gmail.com>
2023-06-02 15:56:40 +0900

    [Add Feature] hasDuplicateWord(String cryptogram) 해당 문자열이 중복되는 문자가 있는지 확인하는 함수 추가 - 문제에 예시로 주어졌던 암호 해독 순서를 진행시킬 수 있는 함수가 될 것. - 중복되는 문자가 없다면 바로 종료하도록 함.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,20 +5,32 @@ public class Problem2 {
         String answer = "answer";
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < cryptogram.length(); i++) {
-            char currentCharacter = cryptogram.charAt(i);
-            if (sb.length() == 0) {
-                sb.append(currentCharacter);
-                continue;
-            }
+//        while (hasDuplicateWord(cryptogram)) {
+//
+//        }
 
-            char previousCharacter = sb.charAt(sb.length() - 1);
-
-            if (previousCharacter == currentCharacter) sb.deleteCharAt(sb.length() - 1);
-            else sb.append(currentCharacter);
-        }
-        answer = sb.toString();
+//        for (int i = 0; i < cryptogram.length(); i++) {
+//            char currentCharacter = cryptogram.charAt(i);
+//            if (sb.length() == 0) {
+//                sb.append(currentCharacter);
+//                continue;
+//            }
+//
+//            char previousCharacter = sb.charAt(sb.length() - 1);
+//
+//            if (previousCharacter == currentCharacter) sb.deleteCharAt(sb.length() - 1);
+//            else sb.append(currentCharacter);
+//        }
+//        answer = sb.toString();
 
         return answer;
+    }
+
+    public static boolean hasDuplicateWord(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) return true;
+        }
+
+        return false;
     }
 }
