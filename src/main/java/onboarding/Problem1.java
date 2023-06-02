cdfccb4d9a77f0441527398d59762b686c799a86
2384320 <chdms1549@gmail.com>
2023-06-02 15:45:43 +0900

    [Add Feature & Fix Feature]
    Add Feature
    1. 예외사항에 페이지 범위를 체크해주는 기능 추가
    - 1~400p 안에서만 계산이 이루어지도록 함.
    
    Fix Feature
    1. 페이지 두 쪽에 대한 자릿수 연산 함수를 페이지 하나에 대한 함수로 간추림.
    - while이 두 개 있던 구조를 변경함.
    - 페이지 두 개에 대한 연산을 한 개에 대한 연산으로 바꿔서 재활용성을 높임.
    2. main 함수 가독성 올림.
    - 선수의 score를 구하는 부분이 가독성이 좋지 않아서 함수 처리함.
    - getBiggerScore가 getPagePlusValue와 getPageMultiValue를 필요할 때마다 호출할 수 있도록 로직 변경
    - 두 선수가 똑같은 함수를 같은 의미로 사용할 수 있도록 변경함.
    
    - 두 선수의 점수를 통해 승패를 가리는 코드도 함수 처리함.
    - 이제는 getWinner 함수를 통해 두 선수의 승패를 가릴 수 있도록 함.
    - 단, 함수를 보고 어떤 상황인지를 표현하기 위해서 상수는 그대로 뒀음.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -20,56 +20,69 @@ class Problem1 {
         if (!checkPage(pobiPage1, pobiPage2)
                 || !checkPage(crongPage1, crongPage2)) return exception;
 
-        int pobiScore = Math.max(
-                getPagePlusValue(pobiPage1, pobiPage2),
-                getPageMultiValue(pobiPage1, pobiPage2)
-        );
-        int crongScore = Math.max(
-                getPagePlusValue(crongPage1, crongPage2),
-                getPageMultiValue(crongPage1, crongPage2)
-        );
+        // 각 선수의 점수 측정
+        int pobiScore = getBiggerScore(pobiPage1, pobiPage2);
+        int crongScore = getBiggerScore(crongPage1, crongPage2);
 
-        if (pobiScore > crongScore) answer = pobiWin;
-        else if (pobiScore < crongScore) answer = crongWin;
-        else answer = draw;
+        // 각 선수의 점수를 통해 승자 결정 (무승부 가능)
+        answer = getWinner(pobiScore, crongScore);
 
         return answer;
     }
 
-    public static int getPagePlusValue(int page1, int page2) {
-        int plusPage = 0;
-        while (page1 > 0) {
-            plusPage += page1 % 10;
-            page1 /= 10;
-        }
+    private static int getWinner(int pobiScore, int crongScore) {
+        // 두 선수의 점수를 통해 승패를 가림
+        /* 승패에 대한 변수는 상수로 표현
+        * pobiWin = 1;
+        * crongWin = 2;
+        * draw = 0;
+        */
+        if (pobiScore > crongScore) return pobiWin;
+        else if (pobiScore < crongScore) return crongWin;
+        else return draw;
+    }
 
-        while (page2 > 0) {
-            plusPage += page2 % 10;
-            page2 /= 10;
+    private static int getBiggerScore(int page1, int page2) {
+        // 각 선수의 페이지 정보를 통해 덧셈 값이랑 곱셈 값 계산
+        int plusValue = getPagePlusValue(page1) + getPagePlusValue(page2);
+        int multiValue = getPageMultiValue(page1) * getPageMultiValue(page2);
+
+        // 둘 중 더 큰 값의 선수의 점수로 반환
+        return Math.max(plusValue, multiValue);
+    }
+
+    private static int getPagePlusValue(int page) {
+        // 페이지에 대한 자릿수 덧셈 연산
+        int plusPage = 0;
+
+        while (page > 0) {
+            plusPage += page % 10;
+            page /= 10;
         }
         return plusPage;
     }
 
-    public static int getPageMultiValue(int page1, int page2) {
+    private static int getPageMultiValue(int page) {
+        // 페이지에 대한 자릿수 곱셈 연산
         int multiPage = 1;
 
-        while (page1 > 0) {
-            multiPage *= page1 % 10;
-            page1 /= 10;
-        }
-
-        while (page2 > 0) {
-            multiPage *= page2 % 10;
-            page2 /= 10;
+        while (page > 0) {
+            multiPage *= page % 10;
+            page /= 10;
         }
         return multiPage;
     }
 
-    public static boolean checkPage(int page1, int page2) {
+    private static boolean checkPage(int page1, int page2) {
         // 예외사항 1 -> 왼쪽이 홀수, 오른쪽이 짝수가 아닌 경우
         if (page1 % 2 == 0 || page2 % 2 != 0) return false;
+
         // 예외사항 2 -> 왼쪽과 오른쪽의 차이가 1이 아닌 경우
         if (page2 - page1 != 1) return false;
+
+        // 예외사항 3 -> 페이지 범위가 1~400p 범위을 넘을 경우
+        if (page1 < 1 || page2 > 400) return false;
+
         return true;
     }
 }
