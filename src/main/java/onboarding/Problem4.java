35eb8d4994ef3a1622059907e718ee1cb7cfd1ce
2384320 <chdms1549@gmail.com>
2023-06-02 16:39:47 +0900

    [Fix Feature]
    1. isBlank
    - 해당 글자가 공백일 경우 inputReverse 함수를 거치지 않고 바로 greenFrogString에 저장되도록 함.
    
    2. inputReverse
    - main 코드를 가독성있게 하기 위해 변환하는 코드를 함수 처리함.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,19 +3,29 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        for (char alphabet : word.toCharArray()) {
-            if (alphabet == ' ') {
-                sb.append(' ');
-                continue;
-            }
+        StringBuilder greenFrogString = new StringBuilder();
 
-            if (Character.isLowerCase(alphabet)) alphabet = (char)('z' - alphabet + 'a');
-            else alphabet = (char)('Z' - alphabet + 'A');
-            sb.append(alphabet);
+        for (char alphabet : word.toCharArray()) {
+            // 해당 글자가 공백이 아니면 inputReverse 함수를 거치도록 함.
+            if (!isBlank(alphabet)) alphabet = inputReverse(alphabet);
+            greenFrogString.append(alphabet);
         }
 
-        answer = sb.toString();
+        answer = greenFrogString.toString();
         return answer;
+    }
+
+    private static char inputReverse(char alphabet) {
+        // 해당 문자가 소문자라면 소문자 범위에서 변하도록 했고,
+        // 대문자라면 대문자 범위에서 변하도록 함.
+        // 이 부분에서는 아스키 코드를 활용함.
+        if (Character.isLowerCase(alphabet)) alphabet = (char)('z' - alphabet + 'a');
+        else alphabet = (char)('Z' - alphabet + 'A');
+        return alphabet;
+    }
+
+    private static boolean isBlank(char alphabet) {
+        // 해당 글자가 빈칸이면 true 반환
+        return alphabet == ' ';
     }
 }
