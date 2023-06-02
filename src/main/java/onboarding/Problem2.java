04b56e9b989d35aed2cc8a6621a7600a7bdf3809
2384320 <chdms1549@gmail.com>
2023-06-02 16:23:24 +0900

    [Add Feature]
    1. checkDuplicateWord(String cryptogram)
    - cryptogram의 전 글자와 앞 글자를 비교하여 같은 글자가 나오면 해당 인덱스에 true를 입력하는 boolean[] 배열을 반환하도록 함.
    - 같은 글자를 2개씩 검사하도록 하기 위해 true를 입력한 후 i++을 통해 인덱스를 한 칸 뛰어넘도록 함.
    
    2. deleteDuplicateWord(String cryptogram, boolean[] checkDuplicateWordArray)
    - 위 checkDuplicateWord 함수의 반환값인 checkDuplicateWordArray를 통해 해당 인덱스에 true가 저장되어 있다면 해당 글자는 StringBuilder에 입력하지 않는 구조로 작성함.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,31 +3,54 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        StringBuilder sb = new StringBuilder();
 
-//        while (hasDuplicateWord(cryptogram)) {
-//
-//        }
-
-//        for (int i = 0; i < cryptogram.length(); i++) {
-//            char currentCharacter = cryptogram.charAt(i);
-//            if (sb.length() == 0) {
-//                sb.append(currentCharacter);
-//                continue;
-//            }
-//
-//            char previousCharacter = sb.charAt(sb.length() - 1);
-//
-//            if (previousCharacter == currentCharacter) sb.deleteCharAt(sb.length() - 1);
-//            else sb.append(currentCharacter);
-//        }
-//        answer = sb.toString();
+        // hasDuplicateWord를 통해 cryptogram 안에 중복되는 글자가 존재하는지 확인
+        while (hasDuplicateWord(cryptogram)) {
+            // 중복되는 글자가 어디에 저장되어 있는지 boolean 배열로 나타냄.
+            boolean[] checkDuplicateWordArray = checkDuplicateWord(cryptogram);
+            // boolean 배열의 값을 통해 cryptogram에서 중복되는 글자를 제거함.
+            cryptogram = deleteDuplicateWord(cryptogram, checkDuplicateWordArray);
+        }
+        answer = cryptogram;
 
         return answer;
     }
 
-    public static boolean hasDuplicateWord(String cryptogram) {
-        for (int i = 0; i < cryptogram.length(); i++) {
+    private static String deleteDuplicateWord(
+            String cryptogram,
+            boolean[] checkDuplicateWordArray) {
+        // 중복 글자를 제거한 cryptogram을 저장하는 StringBuilder
+        StringBuilder deleteDuplicateCryptogram = new StringBuilder();
+        int checkDuplicateWordArrayLength = checkDuplicateWordArray.length;
+
+        // checkDuplicateWordArray에서 true가 나오는 부분은 패스하도록 함.
+        // -> 입력하지 않음으로써 중복 글자를 제거
+        for (int i = 0; i < checkDuplicateWordArrayLength; i++) {
+            if (checkDuplicateWordArray[i]) continue;
+            deleteDuplicateCryptogram.append(cryptogram.charAt(i));
+        }
+        return deleteDuplicateCryptogram.toString();
+    }
+
+    private static boolean[] checkDuplicateWord(String cryptogram) {
+        int cryptogramLength = cryptogram.length();
+        boolean[] checkDuplicateWord = new boolean[cryptogramLength];
+
+        // cryptogram에서 중복 문자가 발견된다면
+        // checkDuplicateWord의 해당 인덱스에 true 입력
+        for (int i = 0; i < cryptogramLength - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                checkDuplicateWord[i] = true;
+                checkDuplicateWord[i+1] = true;
+                i++;
+            }
+        }
+        return checkDuplicateWord;
+    }
+
+    private static boolean hasDuplicateWord(String cryptogram) {
+        // cryptogram에서 중복 문자가 발견된다면 바로 true 반환
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
             if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) return true;
         }
 
