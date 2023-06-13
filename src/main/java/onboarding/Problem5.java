d52d2f3a30ea4336cd948d7c76631ab86dd202d4
2384320 <chdms1549@gmail.com>
2023-06-13 20:12:02 +0900

    [Fix Feature] 주석 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,7 +1,6 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
@@ -10,21 +9,16 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
 
         for (int unit : moneyUnit) {
-            // 환전할 수 있는 수를 배열에 저장
             answer.add(exchangeMoney(money, unit));
-            // 환전하고 남은 돈을 거슬러 받음.
             money = getChange(money, unit);
         }
-
         return answer;
     }
     private static int getChange(int money, int unit) {
-        // 거스름돈을 받는 작업
         return money % unit;
     }
 
     private static int exchangeMoney(int money, int unit) {
-        // 소지 금액에서 해당 단위의 돈을 얼만큼 환전할 수 있는지 계산
         return money / unit;
     }
 }
