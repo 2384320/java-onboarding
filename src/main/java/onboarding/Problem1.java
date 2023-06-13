6660ffbef0f04c74c0e4d72ac6f49b7d2ae7c231
2384320 <chdms1549@gmail.com>
2023-06-13 23:42:13 +0900

    [Fix Feature] 상수 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,12 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    private static final int exception = -1;
-    private static final int pobiWin = 1;
-    private static final int crongWin = 2;
-    private static final int draw = 0;
-    private static final int firstPage = 1;
-    private static final int lastPage = 400;
+    private static final int EXCEPTION = -1;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int DRAW = 0;
+    private static final int FIRST_PAGE = 1;
+    private static final int LAST_PAGE = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobiPage1 = pobi.get(0);
@@ -17,7 +17,7 @@ class Problem1 {
         int crongPage2 = crong.get(1);
 
         if (checkPage(pobiPage1, pobiPage2)
-                || checkPage(crongPage1, crongPage2)) return exception;
+                || checkPage(crongPage1, crongPage2)) return EXCEPTION;
 
         int pobiScore = getBiggerScore(pobiPage1, pobiPage2);
         int crongScore = getBiggerScore(crongPage1, crongPage2);
@@ -26,9 +26,9 @@ class Problem1 {
     }
 
     private static int getWinner(int pobiScore, int crongScore) {
-        if (pobiScore > crongScore) return pobiWin;
-        else if (pobiScore < crongScore) return crongWin;
-        return draw;
+        if (pobiScore > crongScore) return POBI_WIN;
+        else if (pobiScore < crongScore) return CRONG_WIN;
+        return DRAW;
     }
 
     private static int getBiggerScore(int page1, int page2) {
@@ -61,6 +61,6 @@ class Problem1 {
     private static boolean checkPage(int page1, int page2) {
         if (page1 % 2 == 0 || page2 % 2 != 0) return true;
         if (page2 - page1 != 1) return true;
-        return page1 < firstPage || page2 > lastPage;
+        return page1 < FIRST_PAGE || page2 > LAST_PAGE;
     }
 }
