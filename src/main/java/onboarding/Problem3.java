006a3b24e81f647c3acb7cd74d88104e7eb55bec
2384320 <chdms1549@gmail.com>
2023-06-13 20:09:34 +0900

    [Fix Feature] 주석 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,8 +5,6 @@ public class Problem3 {
         int answer = 0;
 
         for (int i = 1; i <= number; i++) {
-            // count369를 통해 해당 숫자에 얼만큼의 3 6 9가
-            // 포함되어 있는지 그 수를 반환하도록 함.
             answer += count369(i);
         }
         return answer;
