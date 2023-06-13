3a0322d6062c13366e5b9f2321a6c868841c520c
2384320 <chdms1549@gmail.com>
2023-06-13 23:41:56 +0900

    [Fix Feature] 상수 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    private static final int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
+        int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
         for (int unit : moneyUnit) {
             answer.add(exchangeMoney(money, unit));
